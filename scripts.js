67e0169698155b063e894047cd081f917a9c1560
Sarah <sksmart9@gmail.com>
2021-10-24 09:13:18 -0500

    Char's updates

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 (function() {
-   var headerHeight = document.getElementsByClassName('header')[0].clientHeight;
+  var headerHeight = document.getElementsByClassName('header')[0].clientHeight;
 	var instructions = document.getElementById('instructions');
-	var navItem = document.getElementById('show-instructions');
-	instructions.style.top = headerHeight + 'px';
+	//var navItem = document.getElementById('show-instructions');
+	//instructions.style.top = headerHeight + 'px';
 
-	navItem.addEventListener("click", function() {
-		if(document.getElementsByClassName('show-menu').length === 0) {
-			instructions.classList.add("show-menu");
-		}
-		else {
-			instructions.className = ('')
-		}
+	// navItem.addEventListener("click", function() {
+	// 	if(document.getElementsByClassName('show-menu').length === 0) {
+	// 		instructions.classList.add("show-menu");
+	// 	}
+	// 	else {
+	// 		instructions.className = ('')
+	// 	}
 		
-	});
+	// });
 
 })();
 
